dab7bb23464a3234507c6400aaf27da7e5726e3e
hrintm84 <hrintm.84@gmail.com>
2025-09-29 23:20:03 +0900

    お疲れ様です。ご確認よろしくお願いいたします。

--- curriculum/curriculum/src/components/Animation.jsx
+++ curriculum/curriculum/src/components/Animation.jsx
@@ -11,19 +11,40 @@ const Animation = () => {
 
   useEffect(() => {
     const el = document.querySelector("#animation");
-    const playing = el.animate([
-      {transform: "rotateY(0) translateY(1000%)", opacity: "0"},
-      {transform: "rotateY(630deg) translateY(-500%)", opacity: "1"},
-      {transform: "rotateY(360deg) translateY(1000%)", opacity: "0"},
-    ],{
-      duration: 7000,
+
+    
+    
+
+    for(let i = 0; i < 11; i++){
+      const moji = ["あ", "い", "う", "え", "お", "A", "B", "C", "D", "E", "F", "G"];
+      const mojiRamdomNum = Math.floor(Math.random() * 10);
+
+      const p = document.createElement("p");
+      p.textContent = moji[mojiRamdomNum];
+      el.appendChild(p);
+      const marginRandom = Math.random() * (-100 - -50) + 50 + "%";
+      const marginSide = Math.random() * (0 - 100) + 100;
+      el.style.left = marginSide + "%";
+
+      const playing = el.animate([
+        {transform: "rotateY(0)", transform: "translateY(500%)", opacity: "0", offset: "0"},
+        {transform: "rotateY(630deg)", transform: "translateY(marginRandom)", opacity: "1", offset: "0.2"},
+        {transform: "rotateY(360deg)", transform: "translateY(500%)", opacity: "0", offset: "1.0"},
+      ],{
+        duration: 10000,
+      
   })
-    playing.play();
+  p.textContent = moji[mojiRamdomNum];
+    }
+
+    
   }, []);
+
+  
     
     
   return (
-    <div id='animation'>A</div>
+    <div id='animation'></div>
   )
 }
 
